4b66d11b1dd857a82d05c4499b244c37fbd628aa
Matt Coarr <mcoarr@mitre.org>
2009-11-24 13:58:50 +0000

    this works in the very basic case of having a single classref (it should work with multiple classref's but there's a bug)

--- med_facts_xml_project/src/org/mitre/medfacts/xml/MedFactsXml.java
+++ med_facts_xml_project/src/org/mitre/medfacts/xml/MedFactsXml.java
@@ -5,17 +5,28 @@
 
 package org.mitre.medfacts.xml;
 
+import java.net.URL;
+
 /**
  *
  * @author MCOARR
  */
 public class MedFactsXml {
 
+    public void execute()
+    {
+        String filename = "rules_sample_09.xml";
+        URL resourceUrl = getClass().getResource(filename);
+        System.out.format("file url: %s%n", resourceUrl.toString());
+    }
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    {
+        MedFactsXml medFactsXml = new MedFactsXml();
+        medFactsXml.execute();
     }
 
 }

f835d18641228ff4670dda17ed55ccfba42becf1
Matt Coarr <mcoarr@mitre.org>
2010-05-27 16:55:33 +0000

    temporary mods to allow some bad data for relation type and assertion value to be permitted until the data is fixed

--- med-facts-i2b2/src/org/mitre/medfacts/i2b2/annotation/RelationType.java
+++ med-facts-i2b2/src/org/mitre/medfacts/i2b2/annotation/RelationType.java
@@ -18,7 +18,12 @@ public enum RelationType
   TrNAP("TrNAP", "treatment is not administered because of medical problem"),
   PIP("PIP", "medical problem indicates medical problem"),
   TERP("TeRP", "test reveals medical problem"),
-  TECP("TeCP", "test conducted to investigate medical problem")
+  TECP("TeCP", "test conducted to investigate medical problem"),
+  NONEPP("NONEPP", "NonePP"),
+  NONETRP("NONETRP", "NONETRP"),
+  TRNAP("TRNAP", "TRNAP"),
+  NONETEP("NONETEP", "NONETEP"),
+  UNCERTAINT("UNCERTAINT", "UNCERTAINT")
   ;
 
   protected String shortName;

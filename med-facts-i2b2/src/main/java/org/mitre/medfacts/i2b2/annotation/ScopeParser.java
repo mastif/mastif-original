ef35b529ced1ea35ee025f4d56f122c1b4f90db6
Ben Wellner <wellner@mitre.org>
2010-06-23 19:13:11 +0000

    Added ScopeParser.java to annotation package to invoke the scope/cue parser.

---
+++ med-facts-i2b2/src/main/java/org/mitre/medfacts/i2b2/annotation/ScopeParser.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.mitre.medfacts.i2b2.annotation;
+
+import java.util.List;
+import org.mitre.itc.jcarafe.scopetagger.FullDecoder;
+import org.mitre.itc.jcarafe.scopetagger.IndexedAnnot;
+
+/**
+ *
+ * @author WELLNER
+ */
+public class ScopeParser {
+
+    FullDecoder decoder = null;
+    public ScopeParser(String sModel, String cModel) {
+        decoder = FullDecoder.apply(sModel, cModel);
+
+    }
+
+    public List<ScopeOrCueAnnotation> decodeDocument(String [][] toks) {
+        List<IndexedAnnot> annots1 = decoder.decodeDocument(toks);
+        List<ScopeOrCueAnnotation> annots = null;
+        for (IndexedAnnot a : annots1) {
+            org.mitre.itc.jcarafe.crf.Annotation an = a.a();
+            String typ = an.typ().labelHead();
+            if (typ.equals("scope")) {
+                ScopeAnnotation s = new ScopeAnnotation();
+                s.setScopeId(a.i());
+                annots.add(s);
+            } else {
+                CueAnnotation c = new CueAnnotation();
+                String st = an.typ().assoc("type");
+                if (st.equals("negation"))
+                    c.setCueSubType(CueSubType.NEGATION);
+                else
+                    c.setCueSubType(CueSubType.SPECULATION);
+                c.setScopeIdReference(a.i());
+                annots.add(c);
+            }
+        }
+        return annots;
+    }
+}

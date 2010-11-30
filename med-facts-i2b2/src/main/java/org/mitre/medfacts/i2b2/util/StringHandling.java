8fb2ceed663532409352f9774ab42d1839fad183
Matt Coarr <mcoarr@mitre.org>
2010-11-30 13:42:37 +0000

    added StringHandling.readEntireContents

--- med-facts-i2b2/src/main/java/org/mitre/medfacts/i2b2/util/StringHandling.java
+++ med-facts-i2b2/src/main/java/org/mitre/medfacts/i2b2/util/StringHandling.java
@@ -5,6 +5,10 @@
 
 package org.mitre.medfacts.i2b2.util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -40,6 +44,35 @@ public class StringHandling
   {
     Matcher matcher = ABSOLUTE_FILE_NAME_PREFIX_REGEX.matcher(fileName);
     return matcher.matches();
+  }
+
+  public static String readEntireContents(File modelFile)
+  {
+    try
+    {
+      FileReader fileReader = new FileReader(modelFile);
+      BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+      StringBuilder sb = new StringBuilder();
+
+      char buffer[] = new char[2048];
+
+      int charsRead = 0;
+
+      while ((charsRead = bufferedReader.read(buffer)) >= 0)
+      {
+        sb.append(buffer, 0, charsRead);
+      }
+
+      String result = sb.toString();
+      return result;
+
+    } catch(IOException e)
+    {
+      String message = "IOException while reading model file";
+      throw new RuntimeException(message, e);
+    }
+
   }
 
 }

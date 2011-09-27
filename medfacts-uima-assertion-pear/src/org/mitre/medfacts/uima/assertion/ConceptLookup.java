0796101b5baf21318a3d2c937a8dd70242a50c96
Matt Coarr <mcoarr@mitre.org>
2011-09-27 17:18:29 +0000

    added ctakes test values for TUIs

--- medfacts-uima-assertion-pear/src/org/mitre/medfacts/uima/assertion/ConceptLookup.java
+++ medfacts-uima-assertion-pear/src/org/mitre/medfacts/uima/assertion/ConceptLookup.java
@@ -24,18 +24,31 @@ public class ConceptLookup
   {
     String diseasesAndDisordersTuis[] =
     { "T019", "T020", "T037", "T046", "T047", "T048", "T049", "T050", 
-      "T190", "T191", "T033" };
-    String signAndSymptomTuis[] = { "T184" };
+      "T190", "T191", "T033",
+      // and for testing
+      "T_DD"
+    };
+    String signAndSymptomTuis[] =
+      { "T184",
+    	// and for testing
+    	"T_SS"
+      };
     String anatomicalSitesTuis[] =
       { "T017", "T029", "T023", "T030", "T031", "T022", "T025", "T026",
-        "T018", "T021", "T024" };
+        "T018", "T021", "T024",
+        // and for testing
+        "T_AS"
+      };
     String medicationsAndDrugsTuis[] =
       { "T116", "T195", "T123", "T122", "T118", "T103", "T120", "T104",
         "T200", "T111", "T196", "T126", "T131", "T125", "T129", "T130",
         "T197", "T119", "T124", "T114", "T109", "T115", "T121", "T192",
         "T110", "T127"};
     String proceduresTuis[] =
-      { "T060", "T065", "T058", "T059", "T063", "T062", "T061" };
+      { "T060", "T065", "T058", "T059", "T063", "T062", "T061",
+    	// and for testing
+    	"T_PR"
+      };
     String deviceTuis[] = { "T074", "T075" };
     String laboratoryTuis[] = { "T059" };
     

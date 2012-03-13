c8370fa2703f6f312dcbbc624757fa4ef8ab92c9
Matt Coarr <mcoarr@mitre.org>
2012-03-13 21:17:44 +0000

    fixed issue with the index returning all annotations, not just those of the specified class.

--- medfacts-uima-assertion-pear/src/main/java/org/mitre/medfacts/uima/assertion/CasIndexer.java
+++ medfacts-uima-assertion-pear/src/main/java/org/mitre/medfacts/uima/assertion/CasIndexer.java
@@ -1,10 +1,15 @@
 package org.mitre.medfacts.uima.assertion;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.logging.Logger;
 
 import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.Type;
 import org.apache.uima.cas.text.AnnotationIndex;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
@@ -16,10 +21,12 @@ public class CasIndexer<T extends Annotation>
   private Logger logger = Logger.getLogger(CasIndexer.class.getName());
   private JCas jcas;
   protected Map<Integer, T> mapByAddress;
+  protected Type targetType;
   
-  public CasIndexer(JCas jcas)
+  public CasIndexer(JCas jcas, Type targetType)
   {
     this.jcas = jcas;
+    this.targetType = targetType;
     initialize();
   }
   
@@ -29,7 +36,14 @@ public class CasIndexer<T extends Annotation>
   
   public void initialize()
   {
-    AnnotationIndex<Annotation> annotationIndex = jcas.getAnnotationIndex();
+    AnnotationIndex<Annotation> annotationIndex = null;
+    if (targetType == null)
+    {
+      annotationIndex = jcas.getAnnotationIndex();
+    } else
+    {
+      annotationIndex = jcas.getAnnotationIndex(targetType);
+    }
     
     mapByAddress = new HashMap<Integer, T>();
     
@@ -51,6 +65,47 @@ public class CasIndexer<T extends Annotation>
   public Annotation lookupByAddress(int address)
   {
     return mapByAddress.get(address);
+  }
+  
+  public String convertToDebugOutput(String label, T targetAnnotation)
+  {
+    StringWriter sw = new StringWriter();
+    PrintWriter p = new PrintWriter(sw);
+    
+    if (label == null) label = "";
+    
+    p.format("=== \"%s\" BEGIN [%d] ===%n", label, targetAnnotation.getAddress());
+    
+    Set<Integer> addressSet = new TreeSet<Integer>();
+    if (mapByAddress != null)
+    {
+      addressSet.addAll(mapByAddress.keySet());
+    }
+    
+    if (!addressSet.isEmpty())
+    {
+      for (Integer currentAddress : addressSet)
+      {
+        String highlightIfMatching = currentAddress.equals(targetAnnotation.getAddress()) ? "###" : "";
+        p.format("ANNOTATION: address: %d %s%n", currentAddress, highlightIfMatching);
+        T currentAnnotation = mapByAddress.get(currentAddress);
+        if (currentAnnotation == null)
+        {
+          p.println("  annotation IS NULL");
+        } else
+        {
+          p.format("  class: %s%n", currentAnnotation.getClass().getName());
+          p.format("  value: %s%n", currentAnnotation.toString());
+        }
+      }
+    }
+    
+    p.format("=== \"%s\" END [%d] ===%n", label, targetAnnotation.getAddress());
+    
+    String output = sw.toString();
+    p.close();
+    
+    return output;
   }
 
   public JCas getJcas()
